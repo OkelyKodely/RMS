e1c3a49eba79b3307c9d8e70a57895a18e9a09d5
Daniel Cho <dh.cho428@gmail.com>
2018-09-20 00:20:14 -0700

    Add files via upload

---
+++ cRent.java
@@ -0,0 +1,48 @@
+public class cRent {
+
+    private int flat;
+    private int house;
+    private Double cost;
+    private Double numberofroom;
+    private String location;
+
+    public int getFlat() {
+        return flat;
+    }
+
+    public void setFlat(int flat) {
+        this.flat = flat;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Double getNumberofroom() {
+        return numberofroom;
+    }
+
+    public void setNumberofroom(Double numberofroom) {
+        this.numberofroom = numberofroom;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ oUtilities.java
@@ -0,0 +1,31 @@
+
+public class oUtilities {
+
+    private Double localTax;
+    private Double waterBill;
+    private Double electricity;
+
+    public Double getLocalTax() {
+        return localTax;
+    }
+
+    public void setLocalTax(Double localTax) {
+        this.localTax = localTax;
+    }
+
+    public Double getWaterBill() {
+        return waterBill;
+    }
+
+    public void setWaterBill(Double waterBill) {
+        this.waterBill = waterBill;
+    }
+
+    public Double getElectricity() {
+        return electricity;
+    }
+
+    public void setElectricity(Double electricity) {
+        this.electricity = electricity;
+    }
+}
